a1b38c8444d01a73f98e1cabce1ef98c72e8093c
Yu Hua Cheng <ycheng@pulsepoint.com>
2018-02-13 20:31:32 -0800

    credential safety (#3)

--- one_time_password/functions/twilio_client.js
+++ one_time_password/functions/twilio_client.js
@@ -1,6 +1,7 @@
 const twilio = require('twilio');
+const twilioKeys = require('./twilio_keys');
 
-const accountSid = 'ACe49728cc2a5eab331de67a1202c9b47f';
-const authToken = 'c09cdf428e816de02f6f2725a0c0db49';
+const accountSid = twilioKeys.accountSid;
+const authToken = twilioKeys.authToken;
 
 module.exports = new twilio.Twilio(accountSid, authToken);
